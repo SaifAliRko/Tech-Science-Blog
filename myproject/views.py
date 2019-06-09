0fc95fb717130b5fbb58c2d49e8a7147e140e7c6
sa23587 <saif.ali2@stud.th-deg.de>
2019-06-10 01:20:09 +0200

    project changed

--- myproject/views.py
+++ myproject/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    #return HttpResponse('index')
-    return render(request,'index.html')
+def homepage(request):
+   # return HttpResponse('homepage')
+   return render(request, 'homepage.html')
+
 def about(request):
-    #return HttpResponse('about')
-    return render(request,'about.html')
+   # return HttpResponse
+   return render(request, 'about.html')
